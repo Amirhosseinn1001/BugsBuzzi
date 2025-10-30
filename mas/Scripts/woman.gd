581badd67357f84e023ab37c6ac86817df942b36
Mahdi <msrjk1384@gmail.com>
2025-10-30 15:53:34 +0330

    enemies added + cannon

---
+++ mas/Scripts/woman.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+var health = 300
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func take_damage(amount: int) ->void:
+	health -= amount
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
