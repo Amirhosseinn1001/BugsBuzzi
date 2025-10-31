494ce252ab9d1edd3f42936a48c041c73eecf3e5
Amirhossein <amirhmoll84@gmail.com>
2025-10-31 11:46:23 +0330

    Add Music

--- mas/Scripts/game.gd
+++ mas/Scripts/game.gd
@@ -1,5 +1,15 @@
 extends Node2D
 
+@onready var music = AudioStreamPlayer.new()
+
 func _ready():
+	# Add intro scene
 	var intro = preload("res://Scenes/IntroScreen.tscn").instantiate()
 	add_child(intro)
+
+	# Setup background music
+	music.stream = load("res://Audio/background_music_1.ogg")
+	music.volume_db = 0
+	music.stream.set_loop(true)
+	add_child(music)
+	music.play()
